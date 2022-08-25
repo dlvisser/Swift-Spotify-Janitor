dd91de320b29e02c8239a0b10aa793ac4efe2978
Dave Visser <dl.visser@protonmail.com>
2022-08-25 21:20:23 +0200

    Initial Commit

---
+++ Swift Spotify Janitor/Swift_Spotify_JanitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Spotify_JanitorApp.swift
+//  Swift Spotify Janitor
+//
+//  Created by Dave Visser on 25/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Spotify_JanitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

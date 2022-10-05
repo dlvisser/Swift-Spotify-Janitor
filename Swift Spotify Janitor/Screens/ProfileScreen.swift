93186c372781b23d1a60b478f019e43dca9c5911
Dave Visser <dl.visser@protonmail.com>
2022-10-05 20:22:48 +0200

    feat(bottomnavbar): first iteration of a bottom navigation bar.

---
+++ Swift Spotify Janitor/Screens/ProfileScreen.swift
@@ -0,0 +1,44 @@
+//
+//  ProfileScreen.swift
+//  Swift Spotify Janitor
+//
+//  Created by Dave Visser on 05/10/2022.
+//
+
+import SwiftUI
+
+struct ProfileScreen: View {
+    var body: some View {
+        ZStack{
+            ZStack(alignment: .top){
+                AppColorConstants.backgroundColor
+                GeometryReader{ geo in
+                    Rectangle()
+                        .fill(AppColorConstants.backgroundGradient)
+                        .frame(width: geo.size.width, height: geo.size.height/3)
+                }
+            }
+            .edgesIgnoringSafeArea(.all)
+            VStack(alignment: .leading){
+                Text("This should be your profile... Right?")
+                    .font(Font.custom("Poppins-ExtraBold", size: 32))
+                    .multilineTextAlignment(.leading)
+                    
+                Spacer()
+                ProfileTag()
+            }
+        }
+        .preferredColorScheme(.dark)
+    }
+}
+
+struct ProfileScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(DevelopmentConstant.previewdevices, id: \.self) { deviceName in
+            ProfileScreen()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+                .environmentObject(ModelData())
+        }
+    }
+}
